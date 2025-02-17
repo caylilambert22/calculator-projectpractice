6dfd980d2f63479c2c768ae1b83072f85b9439af
caylilambert22 <caylilambert@gmail.com>
2025-02-17 14:55:42 -0500

    connecting html to javascript to begin bringing function to calculator

---
+++ calculator-practice.js
@@ -0,0 +1,10 @@
+const inputField = document.getElementById="form_input";
+const submitButton = document.getElementById("submit-button");
+
+//input numbers into field - not complete
+
+
+//submit button - not complete
+submitButton.addEventListener("click", function() {
+  
+})
